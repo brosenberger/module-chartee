c0afbc2fc1d37a3d4344bb58fff69d502c513cd0
Benjamin Rosenberger <rosenberger@e-conomix.at>
2023-11-06 07:20:27 +0100

    :hammer: add chart request parameter service
    * adds single point of getting common request data for adminhtml chart filters (first one with store filter from adminhtml components like dashboard store_filter)

---
+++ Model/ChartRequestParameterService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BroCode\Chartee\Model;
+
+use Magento\Framework\App\RequestInterface;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\StoreManagerInterface;
+
+class ChartRequestParameterService
+{
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @param StoreManagerInterface $storeManager
+     * @param RequestInterface $request
+     */
+    public function __construct(
+        StoreManagerInterface $storeManager,
+        \Magento\Framework\App\RequestInterface $request
+    ) {
+        $this->request = $request;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getStoreIds()
+    {
+        if ($this->getRequest()->getParam('store')) {
+            return [$this->getRequest()->getParam('store')];
+        } elseif ($this->getRequest()->getParam('website')) {
+                return $this->storeManager->getWebsite($this->getRequest()->getParam('website'))->getStoreIds();
+        } elseif ($this->getRequest()->getParam('group')) {
+            return $this->storeManager->getGroup($this->getRequest()->getParam('group'))->getStoreIds();
+        }
+        return null;
+    }
+
+    protected function getRequest()
+    {
+        return $this->request;
+    }
+}
