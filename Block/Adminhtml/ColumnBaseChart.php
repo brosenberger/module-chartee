5f355615a938543a55a463c666be91b6caf99516
Benjamin Rosenberger <rosenberger@e-conomix.at>
2023-11-21 10:46:24 +0100

    :lipstick: add column based format template for charts

---
+++ Block/Adminhtml/ColumnBaseChart.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BroCode\Chartee\Block\Adminhtml;
+
+use Magento\Backend\Block\Template;
+
+class ColumnBaseChart extends Template
+{
+    protected $_template = 'BroCode_Chartee::column_base_chart.phtml';
+    public function getSectionTitle()
+    {
+        return $this->getData('section_title');
+    }
+}
