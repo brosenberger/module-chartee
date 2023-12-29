6fcd258b35f4359c905095fdef903f2840780290
Benjamin Rosenberger <rosenberger@e-conomix.at>
2023-12-29 16:15:53 +0100

    :hammer: add dynamic check of how many children are displayed for width calculation

--- Block/ColumnBaseChart.php
+++ Block/ColumnBaseChart.php
@@ -7,8 +7,47 @@ use Magento\Backend\Block\Template;
 class ColumnBaseChart extends Template
 {
     protected $_template = 'BroCode_Chartee::column_base_chart.phtml';
+
+    protected $_contentHtml = null;
+    protected $_childCount = null;
+
     public function getSectionTitle()
     {
         return $this->getData('section_title');
+    }
+
+    public function getChildCount()
+    {
+        $this->getContentHtml();
+        return $this->_childCount;
+    }
+
+    public function getContentHtml()
+    {
+        if ($this->_contentHtml === null) {
+            $this->_childCount = 0;
+            $layout = $this->getLayout();
+            if (!$layout) {
+                return '';
+            }
+            $contentHtml = '';
+            foreach ($layout->getChildNames($this->getNameInLayout()) as $child) {
+                $childHtml =  $layout->renderElement($child, true);
+                if (!empty($childHtml)) {
+                    $this->_childCount++;
+                }
+                $contentHtml .= $childHtml;
+            }
+            $this->_contentHtml = $contentHtml;
+        }
+        return $this->_contentHtml;
+    }
+
+    public function toHtml()
+    {
+        if ($this->getChildCount() === 0 && empty($this->getContentHtml())) {
+            return '';
+        }
+        return parent::toHtml();
     }
 }
